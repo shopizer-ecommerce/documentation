7c64ac72b51e8c20e09a012f2d4097d24b2540a5
shopizer-ecommerce <contact@shopizer.com>
2023-04-26 01:45:05 +0000

    [skip ci] latest

---
+++ assets/api_index.md.b13188d4.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as a,o as r,R as o}from"./chunks/framework.c3af281d.js";const f=JSON.parse('{"title":"Shopizer API Reference","description":"","frontmatter":{},"headers":[],"relativePath":"api/index.md"}'),t={name:"api/index.md"},i=o("",5),s=[i];function p(n,c,h,l,d,_){return r(),a("div",null,s)}const g=e(t,[["render",p]]);export{f as __pageData,g as default};
